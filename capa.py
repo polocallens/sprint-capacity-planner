b45ebf61f4dab064b1bea330fd1ba0387172742b
paul.callens <paul.callens@swisscom.com>
2023-10-24 11:51:31 +0200

    Add uncertainty factor

--- capa.py
+++ capa.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Function to simulate story points based on input data and capacity
-def simulate_story_points(data, next_PI_capacity, n_simulations=1000):
+def simulate_story_points(data, next_PI_capacity, uncertainty_factor, n_simulations=1000):
     developer_story_points = {dev: [] for dev in data}
     total_story_points = []
 
@@ -12,8 +12,12 @@ def simulate_story_points(data, next_PI_capacity, n_simulations=1000):
 
         for dev, records in data.items():
             ratio = np.mean(np.array(records["story_points"]) / np.array(records["capacity"]))
-            future_presence = next_PI_capacity[dev]
-            future_story_points = future_presence * ratio
+            
+            # Introduce randomness into the future capacity, simulating potential absence
+            random_factor = 1 - np.random.rand() * uncertainty_factor
+            adjusted_future_capacity = next_PI_capacity[dev] * random_factor
+            
+            future_story_points = adjusted_future_capacity * ratio
             developer_story_points[dev].append(future_story_points)
             sprint_story_points += future_story_points
 
@@ -26,6 +30,9 @@ st.title('Sprint Capacity Planner')
 
 # Number of developers
 num_devs = st.number_input('Number of developers', min_value=1, value=4)
+
+# Uncertainty factor for potential absence
+uncertainty_factor = st.slider('Uncertainty factor (e.g., for sickness, unexpected leave)', min_value=0.0, max_value=0.5, value=0.1, step=0.05)
 
 # Input for developers' data
 dev_data = {}
@@ -46,7 +53,7 @@ for i in range(1, num_devs + 1):
 
 # Run simulation button
 if st.button('Calculate Capacity'):
-    dev_story_points, total_capacity = simulate_story_points(dev_data, next_PI_capacity)
+    dev_story_points, total_capacity = simulate_story_points(dev_data, next_PI_capacity, uncertainty_factor)
     st.subheader('Predicted story points per developer')
     for dev, points in dev_story_points.items():
         st.text(f'{dev}: {np.mean(points):.2f} story points')
